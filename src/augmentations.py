5d4fa2d87e0ca3129d3222ebe42de9cd36fd1b3a
Alexander <taa13102016@gmail.com>
2024-10-17 18:44:21 +0300

    May be problem with color after augmentations

--- src/augmentations.py
+++ src/augmentations.py
@@ -8,7 +8,7 @@ def get_train_augmentations(img_width: int, img_height: int) -> alb.Compose:
         alb.VerticalFlip(p=0.5),
         alb.ShiftScaleRotate(),
         alb.GaussianBlur(),
-        alb.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.6),
+        alb.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.2),
         alb.Resize(height=img_height, width=img_width),
         alb.Normalize(),
         ToTensorV2()
