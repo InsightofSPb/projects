4ffcf778c19dd0dc314ce3e7df21ba1c49ac2638
Alexander <taa13102016@gmail.com>
2024-10-19 18:57:41 +0300

    Create small model zoo

---
+++ src/local_model_zoo.py
@@ -0,0 +1,26 @@
+import segmentation_models_pytorch
+
+class UnetModel:
+    def __init__(self, encoder_name = 'resnet 34', encoder_weights='imagenet', in_channels=3, num_cls=1):
+        self.model = segmentation_models_pytorch.Unet(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            in_channels=in_channels,
+            classes=num_cls
+        )
+    
+    def get_model(self):
+        return self.model
+
+
+class DeepLabV3PlusModel:
+    def __init__(self, encoder_name='resnet34', encoder_weights='imagenet', in_channels=3, num_cls=1):
+        self.model = segmentation_models_pytorch.DeepLabV3Plus(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            in_channels=in_channels,
+            classes=num_cls
+        )
+
+    def get_model(self):
+        return self.model
